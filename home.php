59494d7fc625b3a49073552cc9ef544791edf482
farhanbashir06 <farhanbashir06@gmail.com>
2020-01-17 16:41:47 +0500

    updatehome

--- home.php
+++ home.php
@@ -1,20 +1,4 @@
 
-<?php
-echo "farhan";
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <div><h1>Github</h1></div>
-    
-</body>
-</html>
 <!DOCTYPE html>
 <html lang="en">
 <head>
